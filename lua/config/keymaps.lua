4949bbbc54d455b9e13b89006676d29e0a3dda6d
livghit <liviu.g01@gmail.com>
2025-06-12 17:50:15 +0200

    Colorscheme & Keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { desc = "Go to next buffer" })
+vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { desc = "Go to previous buffer" })
---
+++ lua/plugins/github-theme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_light",
+    },
+  },
+}
