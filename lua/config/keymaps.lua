175f04bcf20c317800f01d3a58fb869b4c0c75a1
Liviu Ghita <liviu.ghita@sicor.de>
2025-06-17 07:27:32 +0200

    [Plugins] added new plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { desc = "Go to next buffer" })
+vim.keymap.set("i", "<Esc>", "<Esc>", { noremap = true, silent = true }) -- remove the moving
 vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { desc = "Go to previous buffer" })
+
+vim.keymap.set("n", "<A-k>", "<esc>k", { desc = "Move up" })
+vim.keymap.set("n", "<A-j>", "<esc>j", { desc = "Move down" })
+vim.keymap.set("i", "<A-k>", "<esc>gk", { desc = "Move up" })
+vim.keymap.set("i", "<A-j>", "<esc>gj", { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", "<esc>gk", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", "<esc>gj", { desc = "Move down" })
