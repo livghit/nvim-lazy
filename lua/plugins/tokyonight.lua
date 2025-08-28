6cefa6c4ac8b811bc7d6eeaaf881b3adde438cda
Liviu Ghita <liviu.ghita@sicor.de>
2025-08-28 11:30:54 +0200

    colorschemes added

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        transparency = true,
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
