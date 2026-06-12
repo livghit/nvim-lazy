00cf288036c67d2db8491176f0dad8350afd7d1c
livghit <liviu.g01@gmail.com>
2026-06-12 12:43:54 +0200

    [Feature] ✨ : Add blink.cmp rounded border config

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,19 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    windows = {
+      autocomplete = {
+        border = "rounded",
+        winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+      },
+      documentation = {
+        border = "rounded",
+        winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+      },
+      signature_help = {
+        border = "rounded",
+        winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
+      },
+    },
+  },
+}
